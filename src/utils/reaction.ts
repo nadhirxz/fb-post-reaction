c339add6033bbcb288adec982b417400d677676e
Nadhir <nadhirxz@gmail.com>
2021-11-30 01:19:20 +0100

    Login

--- src/utils/reaction.ts
+++ src/utils/reaction.ts
@@ -2,7 +2,7 @@ import * as puppeteer from 'puppeteer';
 
 const BASE_URL = 'https://www.facebook.com';
 
-export default class ReactionMaker {
+export default class Liker {
 	browser?: puppeteer.Browser;
 	page?: puppeteer.Page;
 
@@ -12,6 +12,29 @@ export default class ReactionMaker {
 		this.browser = await puppeteer.launch({ headless: false });
 		this.page = await this.browser?.newPage();
 		const context = this.browser.defaultBrowserContext();
-		context.overridePermissions(BASE_URL, ['geolocation', 'notifications']);
+		context.overridePermissions('https://www.facebook.com', ['geolocation', 'notifications']);
+	}
+
+	async login() {
+		if (this.page) {
+			await this.page.goto(BASE_URL);
+
+			await this.page.focus('input[name=email]');
+			await this.page.keyboard.type(this.username);
+
+			await this.page.focus('input[name=pass]');
+			await this.page.keyboard.type(this.password);
+
+			await this.page.keyboard.press('Enter');
+
+			await this.page.waitForNavigation();
+
+			await this.page.goto(BASE_URL);
+
+			const loginSuccess = await this.page.evaluate(() => Boolean(document.querySelector('a[aria-label="Home"]')));
+
+			return loginSuccess ? { success: true } : { success: false, error: 'login' };
+		}
+		return { success: false, error: 'init' };
 	}
 }
--- src/utils/start.ts
+++ src/utils/start.ts
@@ -3,5 +3,11 @@ import ReactionMaker from './reaction';
 export async function start(username: string, password: string, post: string) {
 	const liker = new ReactionMaker(username, password, post);
 	await liker.init();
-	// ..
+
+	const { success, error } = await liker.login();
+	if (success) {
+		// ..
+	} else {
+		console.log('error:', error);
+	}
 }

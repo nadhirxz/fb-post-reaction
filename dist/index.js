b690a6cb7f07dc91c5e86468e27d9a1716309fe1
Nadhir <nadhirxz@gmail.com>
2021-12-01 02:30:30 +0100

    Now we can use this as an npm package

--- dist/index.js
+++ dist/index.js
@@ -9,13 +9,15 @@ var commander_1 = require("commander");
 var Reaction_1 = require("./utils/Reaction");
 var ReactionMaker_1 = require("./utils/ReactionMaker");
 require('pkginfo')(module);
-commander_1.program
-    .version(module.exports.version)
-    .description(module.exports.description)
-    .argument('<username>', 'facebook username/email/phone')
-    .argument('<password>', 'facebook password')
-    .argument('<post>', 'facebook post id')
-    .addOption(new commander_1.Option('-r, --reaction <reaction>', 'facebook reaction').choices(__spreadArray([], Reaction_1.reactions)).default('like'))
-    .action(function (username, password, post) { return new ReactionMaker_1.default(username, password, post).react(commander_1.program.opts().reaction); })
-    .parse();
+if (require.main === module) {
+    commander_1.program
+        .version(module.exports.version)
+        .description(module.exports.description)
+        .argument('<username>', 'facebook username/email/phone')
+        .argument('<password>', 'facebook password')
+        .argument('<post>', 'facebook post id')
+        .addOption(new commander_1.Option('-r, --reaction <reaction>', 'facebook reaction').choices(__spreadArray([], Reaction_1.reactions)).default('like'))
+        .action(function (username, password, post) { return new ReactionMaker_1.default(username, password, post).react(commander_1.program.opts().reaction); })
+        .parse();
+}
 exports.default = ReactionMaker_1.default;
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import { reactions } from './utils/Reaction';
 import ReactionMaker from './utils/ReactionMaker';
 require('pkginfo')(module);
 
-program
-	.version(module.exports.version)
-	.description(module.exports.description)
-	.argument('<username>', 'facebook username/email/phone')
-	.argument('<password>', 'facebook password')
-	.argument('<post>', 'facebook post id')
-	.addOption(new Option('-r, --reaction <reaction>', 'facebook reaction').choices([...reactions]).default('like'))
-	.action((username, password, post) => new ReactionMaker(username, password, post).react(program.opts().reaction))
-	.parse();
+if (require.main === module) {
+	program
+		.version(module.exports.version)
+		.description(module.exports.description)
+		.argument('<username>', 'facebook username/email/phone')
+		.argument('<password>', 'facebook password')
+		.argument('<post>', 'facebook post id')
+		.addOption(new Option('-r, --reaction <reaction>', 'facebook reaction').choices([...reactions]).default('like'))
+		.action((username, password, post) => new ReactionMaker(username, password, post).react(program.opts().reaction))
+		.parse();
+}
 
 export default ReactionMaker;

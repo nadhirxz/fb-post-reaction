f182d2d6ca305f5ad2608f24fd5376c83afa73d2
Nadhir <nadhirxz@gmail.com>
2021-12-07 03:17:35 +0100

    Prompt for username/password if not passed as args

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,24 @@
 import { Option, program } from 'commander';
 import { reactions } from './utils/Reaction';
 import ReactionMaker from './utils/ReactionMaker';
+import { prompt } from 'inquirer';
+import { bold } from 'chalk';
 require('pkginfo')(module);
 
 if (require.main === module) {
 	program
 		.version(module.exports.version)
 		.description(module.exports.description)
-		.argument('<username>', 'facebook username/email/phone')
-		.argument('<password>', 'facebook password')
 		.argument('<post>', 'facebook post id')
+		.option('-u, --username <username>', 'facebook username/email/phone')
+		.option('-p, --password <password>', 'facebook password')
 		.addOption(new Option('-r, --reaction <reaction>', 'facebook reaction').choices([...reactions]).default('like'))
-		.action((username, password, post) => new ReactionMaker(username, password, post).react(program.opts().reaction))
+		.action(async post => {
+			let { username, password, reaction } = program.opts();
+			if (!username) username = await prompt({ name: 'username', type: 'input', message: 'username:', validate: value => value.length > 0, prefix: bold.green('-') }).then(({ username }) => username);
+			if (!password) password = await prompt({ name: 'password', type: 'password', mask: '*', message: 'password:', validate: value => value.length > 0, prefix: bold.green('-') }).then(({ password }) => password);
+			new ReactionMaker(username, password, post).react(reaction);
+		})
 		.parse();
 }
 
